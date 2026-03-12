4d1b928cc4a52435b907268ced9c66b91d0f59ed
TheoB82 <grandpatassos@gmail.com>
2026-03-12 17:58:42 +0000

    sdssf

--- app/components/RecipeCard.jsx
+++ app/components/RecipeCard.jsx
@@ -9,11 +9,11 @@ export default function RecipeCard({ recipe }) {
   const title = lang === "GR" ? recipe.TitleGR : recipe.TitleEN;
   const desc = lang === "GR" ? recipe.ShortDescriptionGR : recipe.ShortDescriptionEN;
 
-  return (
-    <Link href={`/recipes/${slugify(recipe.TitleEN)}`}className="block border p-4 rounded shadow hover:shadow-lg">
-      <img src={recipe.Image} className="rounded mb-3" />
-      <h3 className="font-bold text-xl">{title}</h3>
-      <p className="text-sm text-gray-600">{desc}</p>
-    </Link>
-  );
+    return (
+      <Link href={`/recipes/${recipe.ShortID}`} className="block border p-4 rounded shadow hover:shadow-lg">
+        <img src={recipe.Image} className="rounded mb-3" />
+        <h3 className="font-bold text-xl">{title}</h3>
+        <p className="text-sm text-gray-600">{desc}</p>
+      </Link>
+    );
 }
